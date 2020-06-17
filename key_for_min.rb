c0e18f87f5406d013d0c1695b142f3fd6436e9e7
Bradley Calkins <b.raders1013@gmail.com>
2020-06-17 20:29:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  smallest value = ""
+name_hash.each do |element|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
+end
+ 
 
 end
